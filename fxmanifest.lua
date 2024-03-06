0dfeacc674cbba5a4e494c21838f4543d544dc7a
OKiez <87883296+OK1ez@users.noreply.github.com>
2024-03-06 17:50:40 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+
+game "gta5"
+
+author "OK1ez"
+version '1.0.0'
+
+lua54 'yes'
+
+
+ui_page 'html/index.html'
+-- ui_page 'http://localhost:3000/' --for dev
+
+client_script {
+  'client/**',
+}
+server_script {
+  "server/**",
+  }
+shared_script {
+  "shared/**",
+  }
+
+files {
+  'html/**',
+}
